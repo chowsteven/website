26105e829b6a30654b12e303f23b076fb74fce32
sc <stevencstuy@gmail.com>
2022-10-13 18:56:19 -0400

    Change technologies order

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,16 +1,16 @@
 import { Technology } from './Technology';
 import {
+  SiTypescript,
+  SiJavascript,
   SiHtml5,
   SiCss3,
-  SiJavascript,
-  SiTypescript,
   SiReact,
   SiNextdotjs,
   SiTailwindcss,
+  SiJest,
   SiNodedotjs,
   SiExpress,
   SiMongodb,
-  SiJest,
   SiPython,
   SiC,
   SiVercel,
@@ -25,17 +25,17 @@ export const Technologies = () => {
     <div className='pl-12 lg:w-96'>
       <p className='text-2xl font-semibold mb-3'>Technologies</p>
       <ul className='grid grid-flow-col grid-rows-9 gap-2 md:grid-rows-6 lg:grid-rows-4 xl:gap-x-4'>
+        <Technology name='TypeScript' icon={<SiTypescript />} />
+        <Technology name='JavaScript' icon={<SiJavascript />} />
         <Technology name='HTML' icon={<SiHtml5 />} />
         <Technology name='CSS' icon={<SiCss3 />} />
-        <Technology name='JavaScript' icon={<SiJavascript />} />
-        <Technology name='TypeScript' icon={<SiTypescript />} />
         <Technology name='React' icon={<SiReact />} />
         <Technology name='NextJS' icon={<SiNextdotjs />} />
         <Technology name='TailwindCSS' icon={<SiTailwindcss />} />
+        <Technology name='Jest' icon={<SiJest />} />
         <Technology name='NodeJS' icon={<SiNodedotjs />} />
         <Technology name='Express' icon={<SiExpress />} />
         <Technology name='MongoDB' icon={<SiMongodb />} />
-        <Technology name='Jest' icon={<SiJest />} />
         <Technology name='Python' icon={<SiPython />} />
         <Technology name='C' icon={<SiC />} />
         <Technology name='Vercel' icon={<SiVercel />} />
