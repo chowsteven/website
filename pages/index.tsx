e45afabaac5ce4b2bd705dfff9fbffcdfa8f3468
sc <stevencstuy@gmail.com>
2022-10-12 21:50:04 -0400

    Add intro component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Intro } from '../components/Intro';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Steven Chow</title>
         <meta name='description' content='' />
         <link rel='icon' href='/images/favicon.ico' />
       </Head>
 
-      <main></main>
+      <Intro />
 
       <footer></footer>
-    </div>
+    </>
   );
 };
 
