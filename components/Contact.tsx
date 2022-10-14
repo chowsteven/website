8892188713c94e9d5db112b1ad0154db75c632a3
sc <stevencstuy@gmail.com>
2022-10-13 23:31:27 -0400

    Remove email mailto

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,24 +5,25 @@ import { ContactInfo } from './ContactInfo';
 
 export const Contact = () => {
   const contactInfo: { icon: JSX.Element; name: string }[] = [
-    { icon: <SiTwitter size={24} />, name: 'Private!' },
-    { icon: <SiDiscord size={24} />, name: 'Private!' },
-    { icon: <MdPhone size={24} />, name: 'Very private!' },
+    { icon: <MdEmail size={24} />, name: 'Check my resumé!' },
+    { icon: <MdPhone size={24} />, name: 'Check my resumé!' },
+    { icon: <SiTwitter size={24} />, name: 'Private' },
+    { icon: <SiDiscord size={24} />, name: 'Also private' },
   ];
 
   return (
     <div className='pl-12'>
       <p className='text-gray-200 text-2xl font-semibold mb-2'>Contact</p>
       <div className='flex flex-col gap-2'>
-        {/* email */}
+        {/* email
         <div className='flex gap-2'>
           <MdEmail size={24} className='text-gray-200' />
           <Link href='mailto:email@email.com'>
             <a className='text-gray-300'>email@email.com</a>
           </Link>
-        </div>
+        </div> */}
 
-        {/* twitter, discord, phone */}
+        {/* email, twitter, discord, phone */}
         {contactInfo.map((info, i) => (
           <ContactInfo key={i} info={info} />
         ))}
