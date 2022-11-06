e089622dc1689e74ea25d19324f4d06ac7535524
sc <stevencstuy@gmail.com>
2022-11-05 22:58:52 -0400

    Update about text

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,12 @@
 export const About = () => {
   return (
     <div className='flex flex-col lg:items-center pt-32 pb-12 px-12 text-gray-200'>
-      <p className='text-3xl xl:text-5xl mb-4'>
+      <p className='text-3xl font-semibold mb-4 xl:text-5xl'>
         I am a self-taught programmer with a mechanical engineering background.
       </p>
       <p className='text-lg mb-2 xl:text-2xl'>
         I started out developing web applications and recently started exploring
-        desktop applications.
-      </p>
-      <p className='text-lg xl:text-2xl'>
-        That project is still a work in progress, but you can check out my web
-        apps below!
+        desktop applications. Check out my work below!
       </p>
     </div>
   );
